e0c4bf1b645e9d1293c5319d752d263d299ba0fb
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2020-11-25 17:57:40 +0000

    Adding a search feature and code tidying

--- Treehouse-Tech-Degree-Unit-2/js/script.js
+++ Treehouse-Tech-Degree-Unit-2/js/script.js
@@ -1,98 +1,111 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+/**********************************************************************************
+*                                                                                 * 
+*                                                                                 *
+*                       TREEHOUSE TECHDEGREE - UNIT 2                             *
+*                       Data Filtering and Pagination                             *
+*                                                                                 *
+*                                 Matt Hawes                                      *
+*                                                                                 *
+/**********************************************************************************/
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-// variable to reference the HTML 
+//GLOBAL VARIABLE
+const paginationDiv = document.querySelector('.pagination')
 const studentList = document.querySelector(".student-list") 
-
-function showPage(list, page){
-	const pageStart = (page * 9) - 9;   // first page variable
-	const pageEnd = page * 9;           // last page variable
-	studentList.innerHTML = '';
-	
-
-	for(let i = 0; i < list.length; i++){       // iterating through the list to access every student
-		if(i >= pageStart && i < pageEnd){
-       
-       // displaying list in HTML format
-       studentList.insertAdjacentHTML(
-       	"beforeend",       
-        `<li class="student-item cf">
-        <div class="student-details">
-        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-        <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
-        <span class="email">${list[i].email}</span></div>
-        <div class="joined-details">
-        <span class="date">${list[i].registered.date}</span>
-        </div></li>`)
-			
-		}
-	}
-}
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-// variable to reference the HTML
 const ul = document.querySelector(".link-list")
+const header = document.querySelector('.header')
+header.innerHTML =  
+ `<label for="search" class="student-search">
+	<input id="search" placeholder="Search by name...">
+	<button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+	<button class='cancel' type="button">&#x274C</button>
+</label>`
+const searchButton = header.querySelector('button')
+const searchInput = header.querySelector('input')
+const cancelButton = header.querySelector('.cancel')
 
-function addPagination(list){
-    
-			const numOfPages = Math.ceil(list.length/9); // variable to get a total page number and iterate through it
-			for(let i = 1; i <= numOfPages; i++){
-			// display buttons on page
-			ul.insertAdjacentHTML("beforeend", `
-			<li>
-			<button type="button">${i}</button>
-			</li>`) 
-			const buttonOne = document.querySelector("button") // load in with first button 'active'
-			buttonOne.className = "active"
+//CREATES AND APPENDS A PAGE OF 9 STUDENTS
+function showPage(list, page){
+	const pageStart = (page * 9) - 9;                            
+	const pageEnd = page * 9;                           
+	studentList.innerHTML = '';
+	for(let i = 0; i < list.length; i++){                       
+		if(i >= pageStart && i < pageEnd){    
+	       studentList.insertAdjacentHTML("beforeend",     
+	        `<li class="student-item cf">
+		        <div class="student-details">
+		        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+		        <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
+		        <span class="email">${list[i].email}</span></div>
+		        <div class="joined-details">
+		        <span class="date">${list[i].registered.date}</span>
+	        </div></li>`)			
+		};
+	};
+};
 
-		}  
-
-		    
-			//find the active button and display that page
-			ul.addEventListener("click", function(e){
-            
-			const currentPage = e.target;
-			if(currentPage.tagName === "BUTTON"){
+//CREATES AND APPENDS PAGINATION BUTTONS
+function addPagination(list){ 
+	const numOfPages = Math.ceil(list.length/9);                
+	for(let i = 1; i <= numOfPages; i++){		
+		ul.insertAdjacentHTML("beforeend", `
+			<li><button type="button">${i}</button></li>`) 			 
+    };  	
+    const buttonOne = ul.querySelector("button");       
+	buttonOne.className = "active"		    
+	//BUTTON EVENT LISTENER - CHANGES PAGE
+	ul.addEventListener("click", function(e){        
+	const currentPage = e.target;
+		if(currentPage.tagName === "BUTTON"){
 			let notActive = document.querySelector(".active"); 
-			notActive.className = ''           // remove class from non-active buttons
-			currentPage.className = 'active'   // set class to the target
+				notActive.className = ''                                
+				currentPage.className = 'active'                         
 			showPage(data, currentPage.innerHTML)
+		}; 
+	}); 
+ };
 
-		} 
-	}) 
-}
+//CALLBACK FUNCTION TO REMOVE BUTTON CLASS
+function removeActiveClass(){
+ let notActive = document.querySelector(".active"); 
+	 notActive.className = ''                              
+     ul.className = 'active'
+     showPage(data, 1)
+};
 
+//SEARCH FEATURE TO FIND SPECIFIC STUDENT BY FIRST OR LAST NAME
+function findStudent(){
+ 	searchButton.addEventListener('click', function(){
+ 		for(let i = 0; i < data.length; i++){
+            if(searchInput.value.toUpperCase().includes(data[i].name.first.toUpperCase())||
+               searchInput.value.toUpperCase().includes(data[i].name.last.toUpperCase())){
+            	studentList.innerHTML =    
+	        `<li class="student-item cf">
+		        <div class="student-details">
+		        <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
+		        <h3>${data[i].name.title} ${data[i].name.first} ${data[i].name.last}</h3>
+		        <span class="email">${data[i].email}</span></div>
+		        <div class="joined-details">
+		        <span class="date">${data[i].registered.date}</span>
+	        </div></li>`
+	        paginationDiv.removeChild(ul)
+            } 
+            else if(searchInput.value === ''){
+            	paginationDiv.appendChild(ul)           	
+            	removeActiveClass()           	
 
+            }; 		
+ 		}; 					
+ 	});
+ 	//CLEARS SEARCH INPUT AND RETURNS TO FIRST PAGE
+ 	cancelButton.addEventListener('click', function(){
+ 		searchInput.value = ''
+ 		paginationDiv.appendChild(ul)
+ 		removeActiveClass()		
+ 	});
+}; 
 
+//CALL FUNCTIONS
 addPagination(data)
 showPage(data,1)
-
-// Call functions
-
-
-
-
-
-
+findStudent()
 
