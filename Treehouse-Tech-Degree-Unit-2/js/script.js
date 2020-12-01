ddfaf5855effaeb3d14cf0fae6c08cd8cc474ac2
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2020-12-01 10:17:31 +0000

    Search function update

--- Treehouse-Tech-Degree-Unit-2/js/script.js
+++ Treehouse-Tech-Degree-Unit-2/js/script.js
@@ -15,7 +15,12 @@ const studentList = document.querySelector(".student-list")
 const ul = document.querySelector(".link-list")
 const header = document.querySelector('.header')
 const pageNumberMessage = document.createElement('p')
+//IF NO RESULTS ARE FOUND ON SEARCH - MESSAGE
+const noResultMessage = document.createElement('h3')
+noResultMessage.innerHTML = 'No results found! Please try again.'
+noResultMessage.className = 'noResults'
 pageNumberMessage.className = 'pageMessage'
+//HEADER HTML & ATTRIBUTES
 header.innerHTML =  
  `<h2>Students</h2>
 	 <label for="search" class="student-search">
@@ -24,6 +29,54 @@ header.innerHTML =
 const searchButton = header.querySelector('button')
 const searchInput = header.querySelector('input')
 
+//HTML FOR STUDENT LIST
+function generateHtml(list){
+	let html = 
+        `<li class="student-item cf">
+	        <div class="student-details">
+	        <img class="avatar" src="${list.picture.large}" alt="Profile Picture">
+	        <h3>${list.name.title} ${list.name.first} ${list.name.last}</h3>
+	        <span class="email">${list.email}</span></div>
+	        <div class="joined-details">
+	        <span class="date">${list.registered.date}</span>
+        </div></li>`
+    return html
+};
+
+//SEARCH FEATURE TO FIND SPECIFIC STUDENT BY FIRST OR LAST NAME
+searchInput.addEventListener('keyup', function(){
+     let studentPagination = []
+     studentList.innerHTML = ''	    
+		for(let i = 0; i < data.length; i++){
+			if(data[i].name.first.toUpperCase().includes(searchInput.value.toUpperCase())||
+			   data[i].name.last.toUpperCase().includes(searchInput.value.toUpperCase())){
+				studentList.insertAdjacentHTML('beforeend', generateHtml(data[i]))
+				studentPagination.push(data[i])           	
+		        }; 
+			}; 
+		//DISPLAY MESSAGE WHEN NO RESULTS FOUND	
+		if(studentPagination.length > 9){
+		    showPage(studentPagination, 1)
+		    noResultMessage.style.display = 'none';
+		    ul.style.display = 'inherit';
+	    } else if(studentPagination.length === 0){
+	    	body.appendChild(noResultMessage);
+		    noResultMessage.style.display = 'inherit';
+		    ul.style.display = 'none';
+	    } else if(studentPagination.length <= 9){
+	    	noResultMessage.style.display = 'none';
+	    	ul.style.display = 'none';
+	    };
+        //SET PAGINATION TO SEARCH RESULTS
+        if(searchInput.value !== ''){
+	        ul.innerHTML = ''
+		    addPagination(studentPagination)
+	    } else { 
+	        ul.innerHTML = ''
+	    	addPagination(data)
+	    };
+}); 
+
 //CREATES AND APPENDS A PAGE OF 9 STUDENTS
 function showPage(list, page){
 	const pageStart = (page * 9) - 9;                            
@@ -31,15 +84,7 @@ function showPage(list, page){
 	studentList.innerHTML = '';
 	for(let i = 0; i < list.length; i++){                       
 		if(i >= pageStart && i < pageEnd){    
-	       studentList.insertAdjacentHTML("beforeend",     
-	        `<li class="student-item cf">
-		        <div class="student-details">
-		        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-		        <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
-		        <span class="email">${list[i].email}</span></div>
-		        <div class="joined-details">
-		        <span class="date">${list[i].registered.date}</span>
-	        </div></li>`)			
+	       studentList.insertAdjacentHTML("beforeend", generateHtml(list[i]))			
 		};
 	};
 };
@@ -48,13 +93,13 @@ function showPage(list, page){
 function addPagination(list){ 
 	const numOfPages = Math.ceil(list.length/9);                
 	for(let i = 1; i <= numOfPages; i++){		
-		ul.insertAdjacentHTML("beforeend", `
-			<a href="#"><li><button type="button">${i}</button></li></a>`) 			 
-    };  	
+		ul.insertAdjacentHTML("beforeend", 
+			`<a href="#"><li><button type="button">${i}</button></li></a>`			 
+        )};  	
     const buttonOne = ul.querySelector("button");       
 	buttonOne.className = "active";	
 	//SET DEFAULT PAGE NUMBER TO ONE
-	pageNumberMessage.innerHTML = `Page 1 of 5`
+	pageNumberMessage.innerHTML = `Page 1 of ${numOfPages}`
     header.appendChild(pageNumberMessage);	    
 	//BUTTON EVENT LISTENER - CHANGES PAGE
 	ul.addEventListener("click", function(e){        
@@ -64,26 +109,11 @@ function addPagination(list){
 				notActive.className = ''                                
 				currentPage.className = 'active'                         
 			showPage(data, currentPage.innerHTML)
-			pageNumberMessage.innerHTML = `Page ${currentPage.innerHTML} of 5`
+			pageNumberMessage.innerHTML = `Page ${currentPage.innerHTML} of ${numOfPages}`
 		}; 
 	}); 
  };
 
-//SEARCH FEATURE TO FIND SPECIFIC STUDENT BY FIRST OR LAST NAME
-searchInput.addEventListener('keyup', function(){
- 		const listElements = document.querySelectorAll('li')
- 		for(let i = 0; i < listElements.length; i++){
- 			let studentName = listElements[i].firstElementChild.innerHTML;
-        if(studentName.toUpperCase().includes(searchInput.value.toUpperCase())) {
-          listElements[i].style.display = '';
-         } 
-         else {
-          listElements[i].style.display = "none";
-        }
-	};
-}); 					
-
 //CALL FUNCTIONS
 addPagination(data);
 showPage(data,1);
-
